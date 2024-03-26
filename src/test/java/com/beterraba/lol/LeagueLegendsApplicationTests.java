7f7146b88224e3f2a4bd0b65666573a25bcbd819
Beterraba <ls6182315@gmail.com>
2024-03-26 13:30:40 -0300

    initial commit

---
+++ src/test/java/com/beterraba/lol/LeagueLegendsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beterraba.lol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeagueLegendsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
