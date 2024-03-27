e55c23f6776393603f888b7c46198e4b5a7c5eb8
Beterraba <ls6182315@gmail.com>
2024-03-27 15:45:26 -0300

    add ListChampionsUseCase bean

--- src/main/java/com/beterraba/lol/Application.java
+++ src/main/java/com/beterraba/lol/Application.java
@@ -1,7 +1,10 @@
 package com.beterraba.lol;
 
+import com.beterraba.lol.application.ListChampionsUseCase;
+import com.beterraba.lol.domain.ports.ChampionsRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -9,5 +12,15 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+	@Bean
+	public ListChampionsUseCase provideListChampionsUseCase(ChampionsRepository repository) {
+		return new ListChampionsUseCase(repository);
+	}
+
+//	@Bean
+//	public AskChampionUseCase provideAskChampionUseCase(ChampionsRepository repository) {
+//		return new AskChampionUseCase(repository);
+//	}
 
 }
