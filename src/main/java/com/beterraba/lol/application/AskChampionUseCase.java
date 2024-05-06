ad3e0f790d404e6b25cc37f7d9230a45e49e28cf
Beterraba <ls6182315@gmail.com>
2024-05-06 19:59:25 -0300

    create aks champion function

--- src/main/java/com/beterraba/lol/application/AskChampionUseCase.java
+++ src/main/java/com/beterraba/lol/application/AskChampionUseCase.java
@@ -3,13 +3,23 @@ package com.beterraba.lol.application;
 import com.beterraba.lol.domain.exception.ChampionNotFoundException;
 import com.beterraba.lol.domain.model.Champion;
 import com.beterraba.lol.domain.ports.ChampionsRepository;
+import com.beterraba.lol.domain.ports.GenerativeAiService;
 
-public record AskChampionUseCase(ChampionsRepository repository) {
+public record AskChampionUseCase(ChampionsRepository repository, GenerativeAiService genAiService) {
 
     public String askChampions(Long championId, String question) {
 
         Champion champion = repository.findById(championId).orElseThrow(() -> new ChampionNotFoundException(championId));
 
-        return champion.generateContextByQuestion(question);
+        String context = champion.generateContextByQuestion(question);
+
+        String objective = """
+                Atue como um assistente com a habilidade de se comportar como os Campeões do League of Legends (LOL).
+                Responsa perguntas incorporando a personalidade e estilo de um determinado Campeão.
+                Segue a pergunta, o nome do Campeão e sua respectiva lore (história):
+                
+                """;
+
+        return genAiService.generateContent(objective, context);
     }
 }
