35d6d2e2a4415ad9d5c0f0011c9c2fde0738b622
Beterraba <ls6182315@gmail.com>
2024-05-06 19:58:56 -0300

    add crossorigin on listchampion controller

--- src/main/java/com/beterraba/lol/adapters/in/ListChampionsRestController.java
+++ src/main/java/com/beterraba/lol/adapters/in/ListChampionsRestController.java
@@ -3,6 +3,7 @@ package com.beterraba.lol.adapters.in;
 import com.beterraba.lol.application.ListChampionsUseCase;
 import com.beterraba.lol.domain.model.Champion;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import java.util.List;
 @RequestMapping("/champions")
 public record ListChampionsRestController(ListChampionsUseCase useCase) {
 
+    @CrossOrigin
     @GetMapping("/")
     public List<Champion> findAllChampions() {
         return useCase.findAll();
